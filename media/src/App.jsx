5ec5f5bf792a6398bcf0eb881b84e1708b54aca7
karthikeyan <karthikeyanmadhappan@gmail.com>
2025-09-18 16:42:01 +0530

    Loading

---
+++ media/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UsersList from './components/UsersList'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className='container mx-auto'>
+      <UsersList />
+    </div>
+    </>
+  )
+}
+
+export default App
